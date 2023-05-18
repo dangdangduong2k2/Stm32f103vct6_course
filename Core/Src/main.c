330dfa8c9b6dcfd3b2ebccb3a96469e513465a19
botbot1232@gmail.com <122041636+dangdangduong2k2@users.noreply.github.com>
2023-05-18 15:43:12 +0700

    bai tap 3

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -12,43 +12,110 @@
 //timer
 uint32_t time20ms = 0;
 uint32_t count=0;
-
+int temp_phut;
+int temp_giay;
 //button, 7seg
 int num=0;
 int state1;
 int state2;
+int state3;
+int state4;
+
 int lock;
 uint8_t start_count=0;
 //LCD
 CLCD_Name LCD1;
 char lcd_send[16];
+int gio=14;
+int phut=58;
+int giay=59;
 //uart
 uart1 Uart1;
 char  buff[2];
 char char_count[7];
 char ms[]='ms';
 
+
+
 //timer interrupt
 void TIM1_UP_IRQHandler(void)
 {
 
-    if (++ time20ms > 20)
+    if (++ count == 999)
     {
-        time20ms = 0;
+        count=0;
+        giay++;
+        if(59<giay)
+        {
+            giay=0;
+            phut++;
+            if(59<phut)
+            {
+              phut=0;
+              gio++;
+            }
+        }
+        
         
     }
     else
     {
-        if(start_count==1)
-        {
-          count++;
+        if((data&(1<<0))== 0 && state1==0)
+        {   
+          phut++;
+          state1=1;
         }
+        if((data&(1<<5))== 0 && state2==0)
+        {
+          phut--;
+          state2=1;
+        }
+        if((data&(1<<6))== 0 && state3==0)
+        {
+          gio++;
+          state3=1;
+        }
+        if((data&(1<<7))== 0 && state4==0)
+        {
+          gio--;
+          state4=1;
+        }
+        
+        
+        if((data&(1<<0))==1 )
+        {
+          state1=0;
+        }
+        if((data&(1<<5))==(1<<5))
+        {
+          state2=0;
+        } 
+        if((data&(1<<6))==(1<<6))
+        {
+          state3=0;
+        } 
+        if((data&(1<<7))==(1<<7))
+        {
+          state4=0;
+        } 
+        
+        if(temp_phut==phut && temp_giay==giay)
+        {
+          gpio_write(GPIOE,3,0);
+        }
+        else
+        {
+          gpio_write(GPIOE,3,1);
+        }
+        
     }
     TIM1->SR &= ~(TIM_SR_UIF);
 }
 //uart interrupt
 void USART1_IRQHandler(void) 
 {
+    
+    //nhan data
     if (USART1->SR & USART_SR_RXNE) 
     {
         Uart1.data = USART1->DR;
@@ -60,15 +127,30 @@ void USART1_IRQHandler(void)
         else 
         {
             Uart1.buffer[Uart1.index++] = Uart1.data;
+            if (Uart1.index == 1) 
+            {  
+                
+                temp_phut=phut;
+                temp_giay=giay;
+            }
             if (Uart1.index >= 5) 
             {  
                 Uart1.index = 0;
+                
+                
             }
         }
         int temp0 = Uart1.buffer[0]-'0';
         int temp1 = Uart1.buffer[1]-'0';
-        num=temp0*10+temp1;
+        phut=temp0*10+temp1;
+        
+        
+        int temp3 = Uart1.buffer[3]-'0';
+        int temp4 = Uart1.buffer[4]-'0';
+        giay = temp3*10+temp4;  
+        
     } 
+    
 }
 int main(void)
 {
@@ -95,6 +177,7 @@ int main(void)
     //led 7 
     ic_74HC594_init();
     //I/O init:
+    gpio_init(GPIOE,3,0);//loa
     
     //lcd setup
     gpio_init(GPIOD,0,0);
@@ -116,41 +199,62 @@ int main(void)
 while (1)
 {
         shiftIn();
-        if((data&(1<<0))== 0 && lock == 0)
-        {     
-            start_count=1;
-            if(state1==1)
-            {
-              count=0;
-            }
-            else
-            {
-              lock=1;
-            }
-            
-        }
-        if((data&(1<<5))== 0 && state2 ==0)
+ /////////////////////////////
+        if(gio<10)
         {
-            start_count=0;
-            state1=1;
-            state2=1;
-            sprintf(char_count, "%dms\n", count,ms);
-            uart_sendstring(char_count);
+          CLCD_SetCursor(&LCD1,0,0);
+          CLCD_WriteString(&LCD1,"0");
+          
+          sprintf(lcd_send,"%d",gio);
+          CLCD_SetCursor(&LCD1,1,0);
+          CLCD_WriteString(&LCD1,lcd_send);
         }
-        if((data&(1<<5))== (1<<5))
+        else if(10<=gio)
         {
-            
-            state2=0;
-           
+          sprintf(lcd_send,"%d",gio);
+          CLCD_SetCursor(&LCD1,0,0);
+          CLCD_WriteString(&LCD1,lcd_send);
         }
-        if((data&(1<<0))== (1<<0))
-        {
-            
-            lock=0;
-           
-        }
-        quetled(count);
+        CLCD_SetCursor(&LCD1,2,0);
+        CLCD_WriteString(&LCD1,":");
 
+        if(phut<10)
+        {
+          CLCD_SetCursor(&LCD1,3,0);
+          CLCD_WriteString(&LCD1,"0");
+          
+          sprintf(lcd_send,"%d",phut);
+          CLCD_SetCursor(&LCD1,4,0);
+          CLCD_WriteString(&LCD1,lcd_send);
+        }
+        else if(10<=phut)
+        {
+          sprintf(lcd_send,"%d",phut);
+          CLCD_SetCursor(&LCD1,3,0);
+          CLCD_WriteString(&LCD1,lcd_send);
+        }
+        CLCD_SetCursor(&LCD1,5,0);
+        CLCD_WriteString(&LCD1,":");
+        
+        
+        if(giay<10)
+        {
+          CLCD_SetCursor(&LCD1,6,0);
+          CLCD_WriteString(&LCD1,"0");
+          
+          sprintf(lcd_send,"%d",giay);
+          CLCD_SetCursor(&LCD1,7,0);
+          CLCD_WriteString(&LCD1,lcd_send);
+        }
+        else if(10<=giay)
+        {
+          sprintf(lcd_send,"%d",giay);
+          CLCD_SetCursor(&LCD1,6,0);
+          CLCD_WriteString(&LCD1,lcd_send);
+        }
+        CLCD_SetCursor(&LCD1,8,0);
+        CLCD_WriteString(&LCD1,"!");
+       
 }
   
 }
